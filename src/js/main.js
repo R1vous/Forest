0999c1e6c1ae942b5ad0358a81e561abcd503bfa
Bartosz Siejak <bartosz.siejak@gmail.com>
2024-03-04 19:49:59 +0100

    Created navigation ui and js scripts

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,24 @@
 const burgerIcon = document.querySelector('.main-nav__hamburger');
 const listMenu = document.querySelector('.main-nav__links');
+const hamburgerBars = document.querySelector('.main-nav__hamburger-bars');
+const menuLinks = document.querySelectorAll('.main-nav__link');
 
 const toggleMenu = () => {
 	listMenu.classList.toggle('main-nav__links--show');
+	hamburgerBars.classList.toggle('main-nav__hamburger-bars--is-active');
+};
+const closeMenu = () => {
+	listMenu.classList.remove('main-nav__links--show');
+	hamburgerBars.classList.remove('main-nav__hamburger-bars--is-active');
+};
+
+const clickHandler = e => {
+	console.log(e.target);
+	if (!e.target.classList.contains('li-menu') && !e.target.classList.contains('main-nav__hamburger-bars')) {
+		closeMenu();
+	}
 };
 
 burgerIcon.addEventListener('click', toggleMenu);
+menuLinks.forEach(link => link.addEventListener('click', closeMenu));
+window.addEventListener('click', clickHandler);
